6f520f763fa67bafb199db60fe6fe70701341b2e
Anthony Beckford <abeckford532@gmail.com>
2022-12-15 17:31:32 -0500

    Project 7 on 100DaysofSwift

---
+++ Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Anthony Beckford on 12/13/22.
+//
+
+import Foundation
+
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,15 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Anthony Beckford on 12/13/22.
+//
+
+import Foundation
+
+
+struct Petitions: Codable {
+    
+    var results: [Petition]
+    
+}
